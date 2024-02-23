5b0a7de3142360a61d152cdfb57f320bf3d6a075
embeddedvijay <38591377+embeddedvijay@users.noreply.github.com>
2024-02-23 23:38:22 +0530

    Update settings.py

--- smart_matka/settings.py
+++ smart_matka/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p!=a4p*&vsiwz)-k*nrczk1)a-x&@zz&^=ywlvv8xbff2a_5u9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
