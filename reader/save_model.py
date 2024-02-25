2222602bca79bf4e08815538a4a921887e03f711
embeddedvijay <38591377+embeddedvijay@users.noreply.github.com>
2024-02-25 21:41:30 +0530

    Update save_model.py

--- reader/save_model.py
+++ reader/save_model.py
@@ -1,6 +1,21 @@
 from reader.models import *
+from datetime import datetime
+    
+def time_covert_12hr_to_24hr_format(time_string_12hr,format):
+    # Example 12-hour time string
+    time_string_12hr = time_string_12hr + ' '+ format
+    
+    # Specify the format of the 12-hour time string
+    time_format_12hr = "%I:%M %p"
 
+    # Convert 12-hour time string to a datetime object
+    time_object_12hr = datetime.strptime(time_string_12hr, time_format_12hr)
 
+    # Format the datetime object as a 24-hour time string
+    time_string_24hr = time_object_12hr.strftime("%H:%M")
+    print('what is time is here--->',time_string_24hr)
+    return time_string_24hr
+    
 def save_market_time(sridevi_day_op_time,sridevi_day_cl_time,
                      time_bazar_op_time,time_bazar_cl_time,
                      madhur_day_op_time,madhur_day_cl_time,
@@ -20,33 +35,33 @@ def save_market_time(sridevi_day_op_time,sridevi_day_cl_time,
     
     time_data.sridevi_day_open = sridevi_day_op_time
     time_data.sridevi_day_close = sridevi_day_cl_time
-    time_data.time_bazar_open = time_bazar_op_time
-    time_data.time_bazar_close = time_bazar_cl_time
-    time_data.madhur_day_open = madhur_day_op_time
-    time_data.madhur_day_close = madhur_day_cl_time
-    time_data.milan_day_open =  milan_day_op_time
-    time_data.milan_day_close = milan_day_cl_time
-    time_data.rajdhani_day_open = rajdhani_day_op_time
-    time_data.rajdhani_day_close = rajdhani_day_cl_time
-    time_data.superem_day_open = supreme_day_op_time
-    time_data.superem_day_close = supreme_day_cl_time
-    time_data.kalyan_day_open = kalyan_day_op_time
-    time_data.kalyan_day_close = kalyan_day_cl_time
+    time_data.time_bazar_open = time_covert_12hr_to_24hr_format(time_bazar_op_time,'PM')
+    time_data.time_bazar_close = time_covert_12hr_to_24hr_format(time_bazar_cl_time,'PM')
+    time_data.madhur_day_open = time_covert_12hr_to_24hr_format(madhur_day_op_time,'PM')
+    time_data.madhur_day_close = time_covert_12hr_to_24hr_format(madhur_day_cl_time,'PM')
+    time_data.milan_day_open =  time_covert_12hr_to_24hr_format(milan_day_op_time,'PM')
+    time_data.milan_day_close = time_covert_12hr_to_24hr_format(milan_day_cl_time,'PM')
+    time_data.rajdhani_day_open = time_covert_12hr_to_24hr_format(rajdhani_day_op_time,'PM')
+    time_data.rajdhani_day_close = time_covert_12hr_to_24hr_format(rajdhani_day_cl_time,'PM')
+    time_data.superem_day_open = time_covert_12hr_to_24hr_format(supreme_day_op_time,'PM')
+    time_data.superem_day_close = time_covert_12hr_to_24hr_format(supreme_day_cl_time,'PM')
+    time_data.kalyan_day_open = time_covert_12hr_to_24hr_format(kalyan_day_op_time,'PM')
+    time_data.kalyan_day_close = time_covert_12hr_to_24hr_format(kalyan_day_cl_time,'PM')
     
-    time_data.sridevi_night_open = sridevi_nigh_op_time
-    time_data.sridevi_night_close = sridevi_nigh_cl_time
-    time_data.madhur_night_open = madhur_nigh_op_time
-    time_data.madhur_night_close = madhur_nigh_cl_time
-    time_data.superem_night_open = supreme_nigh_op_time
-    time_data.superem_night_close = supreme_nigh_cl_time
-    time_data.milan_night_open = milan_nigh_op_time
-    time_data.milan_night_close = milan_nigh_cl_time
-    time_data.kalyan_night_open = kalyan_nigh_op_time
-    time_data.kalyan_night_close = kalyan_nigh_cl_time
-    time_data.rajdhani_night_open = rajdhani_nigh_op_time
-    time_data.rajdhani_night_close = rajdhani_nigh_cl_time
-    time_data.main_bazar_open = main_bazar_op_time
-    time_data.main_bazar_close = main_bazar_cl_time
+    time_data.sridevi_night_open = time_covert_12hr_to_24hr_format(sridevi_nigh_op_time,'PM')
+    time_data.sridevi_night_close = time_covert_12hr_to_24hr_format(sridevi_nigh_cl_time,'PM')
+    time_data.madhur_night_open = time_covert_12hr_to_24hr_format(madhur_nigh_op_time,'PM')
+    time_data.madhur_night_close = time_covert_12hr_to_24hr_format(madhur_nigh_cl_time,'PM')
+    time_data.superem_night_open = time_covert_12hr_to_24hr_format(supreme_nigh_op_time,'PM')
+    time_data.superem_night_close = time_covert_12hr_to_24hr_format(supreme_nigh_cl_time,'PM')
+    time_data.milan_night_open = time_covert_12hr_to_24hr_format(milan_nigh_op_time,'PM')
+    time_data.milan_night_close = time_covert_12hr_to_24hr_format(milan_nigh_cl_time,'PM')
+    time_data.kalyan_night_open = time_covert_12hr_to_24hr_format(kalyan_nigh_op_time,'PM')
+    time_data.kalyan_night_close = time_covert_12hr_to_24hr_format(kalyan_nigh_cl_time,'PM')
+    time_data.rajdhani_night_open = time_covert_12hr_to_24hr_format(rajdhani_nigh_op_time,'PM')
+    time_data.rajdhani_night_close = time_covert_12hr_to_24hr_format(rajdhani_nigh_cl_time,'PM')
+    time_data.main_bazar_open = time_covert_12hr_to_24hr_format(main_bazar_op_time,'PM')
+    time_data.main_bazar_close = time_covert_12hr_to_24hr_format(main_bazar_cl_time,'PM')
     
     time_data.save()
     
